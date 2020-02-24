949616d062e4757179d535c1d6e7e3ad5c566aa6
khanhhm.os <tata.namkhanh@gmail.com>
2020-02-24 15:07:54 +0700

    Update code

--- src/main/java/com/mb/util/JWEUtility.java
+++ src/main/java/com/mb/util/JWEUtility.java
@@ -4,42 +4,30 @@ import org.jose4j.jwa.AlgorithmConstraints;
 import org.jose4j.jwe.ContentEncryptionAlgorithmIdentifiers;
 import org.jose4j.jwe.JsonWebEncryption;
 import org.jose4j.jwe.KeyManagementAlgorithmIdentifiers;
-import org.jose4j.jwk.JsonWebKey;
-import org.jose4j.lang.JoseException;
 
 public class JWEUtility {
-    private static final String JWK_JSON = "{\"kty\":\"oct\",\"k\":\"Fdh9u8rINxfivbrianbbVT1u232VQBZYKx1HGAGPt2I\"}";
-    private static JsonWebKey jwk;
 
-    static {
-        try {
-            jwk = JsonWebKey.Factory.newJwk(JWK_JSON);
-        } catch (JoseException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String encrypt(String data) throws JoseException {
+    public static String encrypt(String data) throws Exception {
         JsonWebEncryption senderJwe = new JsonWebEncryption();
 
-        senderJwe.setAlgorithmHeaderValue(KeyManagementAlgorithmIdentifiers.DIRECT);
-        senderJwe.setEncryptionMethodHeaderParameter(ContentEncryptionAlgorithmIdentifiers.AES_128_CBC_HMAC_SHA_256);
-        senderJwe.setKey(jwk.getKey());
+        senderJwe.setAlgorithmHeaderValue(KeyManagementAlgorithmIdentifiers.RSA_OAEP_256);
+        senderJwe.setEncryptionMethodHeaderParameter(ContentEncryptionAlgorithmIdentifiers.AES_256_CBC_HMAC_SHA_512);
+        senderJwe.setKey(GenerateKey.getPublicKey());
         senderJwe.setPayload(data);
 
         return senderJwe.getCompactSerialization();
     }
 
-    public static String decrypt(String jwe) throws JoseException {
+    public static String decrypt(String jwe) throws Exception {
         JsonWebEncryption receiverJwe = new JsonWebEncryption();
 
-        AlgorithmConstraints algConstraints = new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST, KeyManagementAlgorithmIdentifiers.DIRECT);
+        AlgorithmConstraints algConstraints = new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST, KeyManagementAlgorithmIdentifiers.RSA_OAEP_256);
         receiverJwe.setAlgorithmConstraints(algConstraints);
 
-        AlgorithmConstraints encConstraints = new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST, ContentEncryptionAlgorithmIdentifiers.AES_128_CBC_HMAC_SHA_256);
+        AlgorithmConstraints encConstraints = new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST, ContentEncryptionAlgorithmIdentifiers.AES_256_CBC_HMAC_SHA_512);
         receiverJwe.setContentEncryptionAlgorithmConstraints(encConstraints);
 
-        receiverJwe.setKey(jwk.getKey());
+        receiverJwe.setKey(GenerateKey.getPrivate());
         receiverJwe.setCompactSerialization(jwe);
 
 
